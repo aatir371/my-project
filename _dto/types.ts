d953706fd8661bbf49e5638bd62bdbff7159975a
DESKTOP-NCE2VKQ\Aatir <aatir371@gmail.com>
2025-12-04 09:45:13 +0500

    initial commit

---
+++ _dto/types.ts
@@ -0,0 +1,12 @@
+export type ExampleDTO = {
+    name: string;
+    age: number;
+}
+
+export type TaskDTO = {
+    id: number;
+    user_id: number;
+    title: string;
+    due_on: Date;
+    status: string;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,111 @@
-import Image from "next/image";
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Camera } from 'lucide-react';
+import { ExampleDTO } from "../../_dto/types";
+
+
+import { useState, useEffect } from "react";
+import { TaskDTO } from "../../_dto/types";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table"
+
+
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+
+	let [josnTasksArray, setJosnTasksArray] = useState<TaskDTO[]>([]);
+
+	async function getTasks() {
+		try {
+			const response = await fetch('https://gorest.co.in/public/v2/todos');
+			setJosnTasksArray(await response.json());
+			console.log(josnTasksArray);
+			console.log(typeof josnTasksArray);
+			return;
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	}
+
+	// useEffect(() => {
+	// 	getTasks();
+	// }, []);
+
+
+	function handleLoadTaskButton() {
+		getTasks();
+	}
+
+	function handleEditButton(id:number) {
+		console.log("handle Edit Button for ID: " + id);
+	}
+
+	function handleDeleteButton(id:number) {
+		console.log("handle Delete Button for ID: " + id);
+	}
+
+	return (
+		<main className="flex flex-col justify-center items-center bg-background">
+			{/* Heading */}
+			<div className="pb-[50px] pt-[30px]">
+				<h1 className="text-4xl font-bold">Task Project</h1>
+			</div>
+
+			<Button className="w-fit mb-[30px]" variant={"default"} onClick={() => { handleLoadTaskButton() }}> Load Tasks Data </Button>
+
+			{josnTasksArray.length === 0 ? (
+				// show "No data found"
+				<h2 className="text-2xl font-bold">No data found!</h2>
+			) : (
+				// show table with josnTasksArray's value populated
+				// <h2>Data Found</h2>
+				<div className="w-full max-w-[1500px]">
+					<Table>
+						<TableCaption>Task list</TableCaption>
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-[100px]">ID</TableHead>
+								<TableHead>User ID</TableHead>
+								<TableHead>Title</TableHead>
+								<TableHead>Due Date</TableHead>
+								<TableHead>Status</TableHead>
+								<TableHead className="text-center">Action</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{josnTasksArray.map((task) => {
+								return (<TableRow key={task.id}>
+									<TableCell className="font-medium">{task.id}</TableCell>
+									<TableCell>{task.user_id}</TableCell>
+									<TableCell>{task.title}</TableCell>
+									<TableCell>{new Date(task.due_on).toLocaleDateString()}</TableCell>
+									<TableCell>{task.status}</TableCell>
+									<TableCell className="flex flex-row gap-2 justify-center">
+										<Button variant={"secondary"} onClick={() => { handleEditButton(task.id) }}> Edit </Button>
+										<Button variant={"destructive"} onClick={() => { handleDeleteButton(task.id) }}> Delete </Button>
+									</TableCell>
+								</TableRow>
+								);
+							})}
+						</TableBody>
+					</Table>
+				</div>
+			)}
+
+
+			<Camera color="red" size={30} />
+			{/* <p className="text-4xl font-bold">{exampleDtoObject.name}</p> */}
+		</main>
+	)
 }
+
+
+
+
+
+
+
+// const exampleDtoObject: ExampleDTO = {
+// 	name: "aatir",
+// 	age: 26
+// }
